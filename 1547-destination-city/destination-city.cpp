82addaf749f7e131e3fdce88882d630bde1583eb
Vishnu Priya <89837239+VishnupriyaThammina@users.noreply.github.com>
2023-12-16 00:52:56 +0530

    Time: 8 ms (75.96%) | Memory: 11.2 MB (39.00%) - LeetSync

---
+++ 1547-destination-city/destination-city.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  //First Method
+
+string destCity(vector<vector<string>>& paths) 
+    {
+        int m = paths.size();
+        
+        unordered_map<string, int> mp;
+        for(int i = 0; i < m; i++)
+           mp[paths[i][0]]++;
+        
+        for(int i = 0; i < m; i++)
+        {
+            if(mp[paths[i][1]] < 1)
+                return paths[i][1];
+        }
+        
+        return "";   
+    
+    }
+	
+	
+};
