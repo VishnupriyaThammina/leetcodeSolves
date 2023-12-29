724641aa97f74d8e459b4faa2eb0bfd752b3e14d
Vishnu Priya <89837239+VishnupriyaThammina@users.noreply.github.com>
2023-12-29 09:14:11 +0530

    Time: 15 ms (53.18%) | Memory: 11.1 MB (46.54%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // the best solution for two sum 
+        // lets make an unordered map
+        unordered_map<int,int> mp;
+        // we are storing the current number and index if compliment is not found;
+        for(int i = 0; i<nums.size();i++){
+            int comp = target-nums[i];
+            if(mp.find(comp)!=mp.end()){
+                return {mp[comp],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {1,-1};
+    }
+};
