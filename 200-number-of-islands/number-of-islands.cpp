59b10a05ec515e4ca91bac69304431e6658cb54a
Vishnu Priya <89837239+VishnupriyaThammina@users.noreply.github.com>
2023-12-30 14:54:36 +0530

    Time: 34 ms (34.17%) | Memory: 14.6 MB (36.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
 int numIslands(vector<vector<char>>& grid) {
- vector<vector<int>> vis(grid.size(),(vector<int>(grid[0].size())));
+     int n = grid.size();
+ int m = grid[0].size();
+ vector<vector<int>> vis(n,(vector<int>(m)));
  int cc[]={0,0,1,-1};
  int rr[]={1,-1,0,0};
+
  queue<pair<int,int>> q;
  int count=0;
- for(int i = 0; i<grid.size();i++){
-     for(int j = 0; j < grid[i].size();j++){
+ for(int i = 0; i<n;i++){
+     for(int j = 0; j < m;j++){
 if(vis[i][j]==0 && grid[i][j]=='1'){
     q.push({i,j});
     count++;
@@ -20,7 +23,7 @@ if(vis[i][j]==0 && grid[i][j]=='1'){
         for(int k = 0; k<4;k++){
             int nc = c+cc[k];
             int nr = r+rr[k];
-            if(nc>=0 && nc<grid.size() && nr>=0 && nr<grid[0].size() && grid[nc][nr]=='1'&& vis[nc][nr]==0){
+            if(nc>=0 && nc<n && nr>=0 && nr<m && grid[nc][nr]=='1'&& vis[nc][nr]==0){
                 vis[nc][nr]=1;
                 q.push({nc,nr});
             }
