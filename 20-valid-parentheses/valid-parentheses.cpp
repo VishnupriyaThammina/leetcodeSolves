c9d7485f01e3d6e91d5dd97633e3c2dbaa56a964
Vishnu Priya <89837239+VishnupriyaThammina@users.noreply.github.com>
2024-01-04 22:37:56 +0530

    Time: 3 ms (45.40%) | Memory: 7.1 MB (14.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        if(s.length()%2 !=0){
+            return false;
+        }
+        unordered_map<char,char> mp;
+        mp['(']=')';
+        mp['[']=']';
+        mp['{']='}';
+
+        for(int i = 0 ; i <s.length();i++){
+            if(s[i]=='(' || s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }else{
+                if(st.empty()||mp[st.top()]!=s[i]){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
